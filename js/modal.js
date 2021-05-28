50a5c18814f073c0be328e12c5868e8fa219afa4
Vinny <63819739+vincenzoDelVillano@users.noreply.github.com>
2021-05-27 23:50:56 -0400

    Create modal.js

---
+++ js/modal.js
@@ -0,0 +1,39 @@
+const modal = document.getElementById("exitModal");
+
+const span = document.getElementsByClassName("close")[0];
+
+document.addEventListener('mouseout', e => {
+    if (!e.toElement && !e.relatedTarget) {
+        modal.style.display = "block";
+    }
+});
+
+const exitEvent = (e) => {
+  if (!e.toElement && !e.relatedTarget) {
+    document.removeEventListener('mouseout', exitEvent);
+    modal.style.display = "block";
+  }
+};
+
+setTimeout(() => {
+    document.addEventListener('mouseout', exitEvent);
+}, 15000);
+
+const exitEvent = e => {
+  if (!e.toElement && !e.relatedTarget && e.clientY < 5) {
+    document.removeEventListener('mouseout', exitEvent);
+    modal.style.display = "block";
+
+		setCookie('resumeModalSeen', true, 7);
+  }
+};
+
+span.onclick = () => {
+  modal.style.display = "none";
+}
+
+window.onclick = (e) => {
+  if (e.target === modal) {
+    modal.style.display = "none";
+  }
+}
